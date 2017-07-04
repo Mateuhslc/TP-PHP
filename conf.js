abb177e0586c54230f45fa46b387cc9c99335c1e
Lucas Macedo Bruzzi <lucascoltec2016@gmail.com>
2017-07-03 22:12:38 -0300

    Protótipo da página inicial 99% COMPLETO
    Só falta consertar pequenos erros

---
+++ conf.js
@@ -0,0 +1,17 @@
+$(".button-collapse").sideNav({
+	edge: 'right'
+});
+
+$(".dropdown-button").dropdown();
+
+$('.dropdown-button').dropdown('close');
+
+$("#dropdown1").dropdown({
+	//hover: false,
+	belowOrigin: true
+});
+
+$("#dropdown2").dropdown({
+	//hover: false,
+	belowOrigin: true
+});
